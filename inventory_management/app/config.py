ad20b97b5098bfb6c4c569cb91add5c6858c0946
akashgajjala01 <akashgajjala16@gmail.com>
2024-09-28 23:59:31 +1000

    initial commit

---
+++ inventory_management/app/config.py
@@ -0,0 +1,3 @@
+import os
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://akashtask8_user:9SE6dI6MxAJYa0hP04h0B0LYO34zZyzQ@dpg-crrvk2rtq21c73da64r0-a.oregon-postgres.render.com/akashtask8')
---
+++ scripts/terraform/resource-group.tf
@@ -0,0 +1,5 @@
+
+resource "azurerm_resource_group" "akashtask8" {
+  name     = var.app_name
+  location = var.location
+}
